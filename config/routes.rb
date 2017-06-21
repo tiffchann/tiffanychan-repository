e44d88aaf16c80adbbc438717148a124ba11593b
Nebo Devcamp <nebograngerlomond@devcamp.com>
2017-06-21 00:48:26 +0000

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  
+  # Rake routes will show all the resource routes except the ones you don't want.
+  # In this case, it will be a show route. 
+  resources :portfolios, except: [:show]
+  
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   
 # /about instead of /blogs/about/
 #pages is the controller. about is the action.

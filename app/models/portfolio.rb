7962a1298d6cbae6bac0fcc45ccc7b9a11b33bfb
Nebo Devcamp <nebograngerlomond@devcamp.com>
2017-06-26 14:22:42 +0000

    Implemented image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -9,4 +9,13 @@ end
 # This scope covers all the posts with rails on ruby subtitles.
 # This is a simplified version of def self.angular above.
 scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+
+after_initialize :set_defaults
+
+def set_defaults
+   self.main_image ||= "http://placeholder.it/600x400"
+   self.thumb_image ||= "http://placeholder.it/350x200"
 end
+end
+
+

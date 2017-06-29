bd344345316a874ad8c068fc3d8a041bb858f59f
Nebo Devcamp <nebograngerlomond@devcamp.com>
2017-06-29 15:25:00 +0000

    Added session tracker for dynamic source links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
   
+  before_action :set_source
+  
+  def set_source
+    # source is the name of the session
+    # Never put anything secret in the session because it is not secure there.
+    session[:source] = params[:q] if params[:q]
+  end
 end

7d2b7c85efff2ed1eaf9918bceee878a154e6fbc
Nebo Devcamp <nebograngerlomond@devcamp.com>
2017-07-20 15:20:47 +0000

    Implemented initial file uploading system via carrierwave for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,23 +6,27 @@ class Portfolio < ApplicationRecord
     include Placeholder
     
     validates_presence_of :title, :body, :main_image, :thumb_image
+    
+    mount_uploader :thumb_image, PortfolioUploader
+    mount_uploader :main_image, PortfolioUploader
 
-# Displays only the posts with angular as a subtitle.
-def self.angular
-   where(subtitle: 'Angular') 
-   
-end
 
-def self.by_position
-   order("position ASC") 
-end
+    # Displays only the posts with angular as a subtitle.
+    def self.angular
+       where(subtitle: 'Angular') 
+       
+    end
+    
+    def self.by_position
+       order("position ASC") 
+    end
 
 # This scope covers all the posts with rails on ruby subtitles.
 # This is a simplified version of def self.angular above.
-scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
-
-after_initialize :set_defaults
-
+    scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+    
+    after_initialize :set_defaults
+    
     def set_defaults
         # ||= sets the default but allows the your images to change if you want. 
        self.main_image ||= Placeholder.image_generator(height: '600', width: '400')

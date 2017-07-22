3df5120c3c998b75d22f87c7a6e00f00c9eb1d4d
Nebo Devcamp <nebograngerlomond@devcamp.com>
2017-07-22 07:54:46 +0000

    Updated file upload forms and refactored placeholder functionality into view

--- app/models/concerns/placeholder.rb
+++
@@ -1,8 +0,0 @@
-# This is a set_default for the image url for the skill.rb
-module Placeholder
-  extend ActiveSupport::Concern
-  def self.image_generator(height:, width:)
-      # #{height} is a dynamic value. That is why a # is there. 
-    "http://placehold.it/#{height}x#{width}"
-  end
-end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,37 +1,26 @@
 class Portfolio < ApplicationRecord
-    has_many :technologies
-    accepts_nested_attributes_for :technologies, 
-                                reject_if: lambda { |attrs| attrs['name'].blank?}
-    
-    include Placeholder
-    
-    validates_presence_of :title, :body, :main_image, :thumb_image
-    
-    mount_uploader :thumb_image, PortfolioUploader
-    mount_uploader :main_image, PortfolioUploader
+  has_many :technologies
+  accepts_nested_attributes_for :technologies,
+                                reject_if: lambda { |attrs| attrs['name'].blank? }
 
+  validates_presence_of :title, :body
+
+  mount_uploader :thumb_image, PortfolioUploader
+  mount_uploader :main_image, PortfolioUploader
 
     # Displays only the posts with angular as a subtitle.
-    def self.angular
-       where(subtitle: 'Angular') 
-       
-    end
-    
-    def self.by_position
-       order("position ASC") 
-    end
+  def self.angular
+    where(subtitle: 'Angular')
+  end
 
+  def self.by_position
+    order("position ASC")
+  end
 # This scope covers all the posts with rails on ruby subtitles.
 # This is a simplified version of def self.angular above.
-    scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
-    
-    after_initialize :set_defaults
-    
-    def set_defaults
-        # ||= sets the default but allows the your images to change if you want. 
-       self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-       self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-    end
+  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
 end
 
+    
 
+ 

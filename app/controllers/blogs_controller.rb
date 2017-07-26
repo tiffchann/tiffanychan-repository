39fb7781a9c965fccc0feb12a5f78d47260a9c11
Nebo Devcamp <nebograngerlomond@devcamp.com>
2017-07-26 15:36:56 +0000

    Implemented action cable for comments

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -15,6 +15,9 @@ class BlogsController < ApplicationController
   def show
     @blog = Blog.includes(:comments).friendly.find(params[:id])
     @comment = Comment.new
+
+    @page_title = @blog.title
+    @seo_keywords = @blog.body
   end
 
   # GET /blogs/new

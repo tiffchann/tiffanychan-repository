639bdd6f0be0ca69fcc688f3398fe660583fce4a
Nebo Devcamp <nebograngerlomond@devcamp.com>
2017-07-26 14:59:38 +0000

    Fixed Git merge conflicts

--- lib/social_tool.rb
+++ lib/social_tool.rb
@@ -1,4 +1,5 @@
 module SocialTool
+<<<<<<< HEAD
   def self.twitter_search
     client = Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV.fetch("TWITTER_CONSUMER_KEY")
@@ -11,4 +12,18 @@ module SocialTool
       "#{tweet.user.screen_name}: #{tweet.text}"
     end
   end
+=======
+    def self.twitter_search
+        client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV.fetch("TWITTER_CONSUMER_KEY")
+        config.consumer_secret     = ENV.fetch("TWITTER_CONSUMER_SECRET")
+        config.access_token        = ENV.fetch("TWITTER_ACCESS_TOKEN")
+        config.access_token_secret = ENV.fetch("TWITTER_ACCESS_SECRET")
+        end 
+        
+        client.search("#kpop", result_type: 'recent').take(6).collect do |tweet|
+            "#{tweet.user.screen_name}: #{tweet.text}"
+        end
+    end
+>>>>>>> ee896a3524c8240c2069d03944a9df435f9244aa
 end
